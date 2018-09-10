f095dbe95e0c522116c2276dd70f1c5c52bc8430
JacobPWeb <jacobpwebdevelopment@outlook.com>
2018-09-10 20:17:45 +0100

    Added pages and fixed errors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = Express();
-
-app.get('/', homepage);
+const app = express();
+const pagesBase = `${__dirname}/pages`
+const [homepageref, aboutpageref, portfolioref] = [`${pagesBase}/homepage`, `${pagesBase}/aboutpage`, `${pagesBase}/portfoliopage`]
 
 const homepage = (req, res) => {
-  res.send('hello world');
+  res.sendFile(`/${homepageref}/home.html`);
+}
+const aboutpage = (req, res) => {
+  res.sendFile()
+}
+const portfoliopage = (req, res) => {
+  res.sendFile()
 }
 
+app.get('/', homepage);
 app.listen(8080, () => console.log('listening on port 8080'));
